ab95011b6e1879141a95fef680090454675a715c
unknown <nithinkv1995@gmai.com>
2026-06-16 21:28:13 +0530

    hello world chain

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    information = """Born into the Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded Zip2, a web software company. Following its sale in 1999, he co-founded X.com, an e-commerce payment system that merged with Confinity in March 2000 to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002."""
+
+    information_template = """
+    given the information {information} about a person, i want to create the below information:
+    1. a short summary
+    2. 2 interesting facts about them"""
+    summary_prompt_template = PromptTemplate(template=information_template, input_variables=["information"])
+
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    chain = summary_prompt_template | llm
+    response = chain.invoke({"information": information})
+    print(response.content)
+
+if __name__ == "__main__":
+    main()
